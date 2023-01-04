c531ed074b077da0378442835bdbc62d6afcdd00
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2023-01-04 22:22:20 +0100

    create header

---
+++ components/Header/Searchbar/index.js
@@ -0,0 +1,23 @@
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import testDb from "../../helpers/testdb.json";
+
+function Searchbar() {
+  return (
+    <Stack width={300}>
+      <Autocomplete
+        id="search"
+        freeSolo
+        options={testDb.map((option) => option.title)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="suchen"
+          />
+        )}
+      />
+    </Stack>
+  );
+}
+export default Searchbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
-import GlobalStyles from "../components/GlobalStyles";
+import GlobalStyles from "../components/Style/GlobalStyles";
+import Layout from "../components/Layout"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
