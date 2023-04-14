6884667aeb83f696e71f19f677348e46fba0e355
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2023-04-15 00:29:38 +0200

    fetch product, article, vpe from mysql server--docker

--- pages/api/getdata/[category]/[id]/index.js
+++ pages/api/getdata/[category]/[id]/index.js
@@ -4,7 +4,29 @@ async function handlerById(req, res) {
   const { category, id } = req.query; // Extrahieren Sie den Wert von 'id' aus der URL
   try {
     const dbconnection = await createDbConnection();
-    const query = `SELECT * FROM product WHERE category = ? AND product_id = ?`;
+    const query = `
+    
+  SELECT product.*, 
+    JSON_ARRAYAGG(
+      JSON_OBJECT(
+        'article_id', article.article_id, 
+        'article_number', article.article_number,
+        'article_name', article.article_name, 
+        'article_description', article.article_description,
+        'vpe', (
+          SELECT JSON_ARRAYAGG(vpe.vpe_value)
+          FROM article_vpe_connection
+          LEFT JOIN vpe ON article_vpe_connection.vpe_id = vpe.vpe_id
+          WHERE article_vpe_connection.article_id = article.article_id
+          group by article.article_number
+        )
+      )
+    ) AS articles
+  FROM product
+  LEFT JOIN article ON product.product_id = article.product_id
+  WHERE product.category = ? AND product.product_id = ?
+  GROUP BY product.product_id`;
+
     const values = [`${category}`, `${id}`];
     const [data] = await dbconnection.execute(query, values);
     dbconnection.end();
@@ -14,3 +36,29 @@ async function handlerById(req, res) {
   }
 }
 export default handlerById;
+
+// const query = `SELECT *
+//     FROM (product
+//     LEFT JOIN article ON product.product_id = article.product_id)
+//     LEFT JOIN article_vpe_connection ON article.article_id = article_vpe_connection.article_id
+//     LEFT JOIN vpe ON article_vpe_connection.vpe_id = vpe.vpe_id
+
+//     WHERE product.category = ? AND product.product_id = ?`;
+
+//   const query = `
+//   SELECT product.*,
+//    JSON_ARRAYAGG(JSON_OBJECT(
+//    'article_id', article.article_id,
+//    'article_name', article.article_name,
+//    'article_description',article.article_description,
+
+//    ))AS articles,
+//    SELECT articles.*
+//    JSON_ARRAYAGG( 'vpe_value', vpe.vpe_value) AS VPE
+//  FROM product
+//  LEFT JOIN article ON product.product_id = article.product_id
+//  LEFT JOIN article_vpe_connection ON article.article_id = article_vpe_connection.article_id
+//  LEFT JOIN vpe ON article_vpe_connection.vpe_id = vpe.vpe_id
+
+//  WHERE product.category = ? AND product.product_id = ?
+//  GROUP BY product.product_id , article.article_id`;
--- pages/product/[category]/[id]/index.js
+++ pages/product/[category]/[id]/index.js
@@ -16,29 +16,29 @@ function Product() {
       async function getDataById() {
         const response = await fetch(apiURL);
         const res = await response.json();
-        setProduct(res.products[0]);
+        setProduct(res.products);
       }
       getDataById();
     } catch (error) {
-      console.log("fehler bljad" + error.message);
+      console.log("Fehler Produkt" + error.message);
     }
   }, []);
 
   console.log("product", product);
-  const {
-    product_id: Id,
-    product_name: name,
-    product_description1: description1,
-    product_description2: description2,
-    product_description3: description3,
-    product_material: material,
-    product_imagepath: image,
-    category: cat,
-  } = product;
+  // const {
+  //   product_id: Id,
+  //   product_name: name,
+  //   product_description1: description1,
+  //   product_description2: description2,
+  //   product_description3: description3,
+  //   product_material: material,
+  //   product_imagepath: image,
+  //   category: cat,
+  // } = product;
   return (
     <>
-      <h2>test product {Id}</h2>
-      <p>name:{name}</p>
+      <h2>test product {id}</h2>
+      {/* <p>name:{name}</p>
       <p>category:{cat}</p>
       <p>material:{material}</p>
       <p>description1:{description1}</p>
@@ -47,7 +47,7 @@ function Product() {
       <img
         src={image}
         alt=""
-      />
+      /> */}
     </>
   );
 }
