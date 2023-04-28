c46c3c5c78d70f40b8e259c3005bbb235c8fdcef
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2023-04-28 21:00:30 +0200

    create link

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,14 +1,24 @@
 import styled from "styled-components";
-
+import Link from "next/link";
 function Navbar() {
   return (
     <nav>
       <StyledList>
-        <li>Möbelbereich</li>
-        <li>Halterungen</li>
-        <li>Wasserbereich</li>
-        <li>Lüftungsbereich</li>
-        <li>Elektrobereich</li>
+        <li>
+          <Link href="/moebel">Möbelbereich</Link>
+        </li>
+        <li>
+          <Link href="/halterung">Halterungen</Link>
+        </li>
+        <li>
+          <Link href="/wasser">Wasserbereich</Link>
+        </li>
+        <li>
+          <Link href="/lueftung">Lüftungsbereich</Link>
+        </li>
+        <li>
+          <Link href="/elektro">Elektrobereich</Link>
+        </li>
       </StyledList>
     </nav>
   );
