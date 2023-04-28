c35d5a0ea2e3bd3ea44acbc89cc7290bd1a69337
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2023-04-28 23:08:22 +0200

    style home main

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,6 +1,7 @@
 import Header from "../Header";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import styled from "styled-components";
 
 function Layout({ children }) {
   return (
