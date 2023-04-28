5b9db90a8bd61e76fb489fcf063ed6bca6697160
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2023-04-28 21:15:07 +0200

    create link

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,25 +1,39 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 function Navbar() {
+  const [isDisplayed, setIsDisplayed] = useState(true);
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setIsDisplayed(!isDisplayed);
+    }
+  }, []);
+
   return (
     <nav>
-      <StyledList>
-        <li>
-          <Link href="/moebel">Möbelbereich</Link>
-        </li>
-        <li>
-          <Link href="/halterung">Halterungen</Link>
-        </li>
-        <li>
-          <Link href="/wasser">Wasserbereich</Link>
-        </li>
-        <li>
-          <Link href="/lueftung">Lüftungsbereich</Link>
-        </li>
-        <li>
-          <Link href="/elektro">Elektrobereich</Link>
-        </li>
-      </StyledList>
+      {isDisplayed && (
+        <StyledList>
+          <li>
+            <Link href="/moebel">Möbelbereich</Link>
+          </li>
+          <li>
+            <Link href="/halterung">Halterungen</Link>
+          </li>
+          <li>
+            <Link href="/wasser">Wasserbereich</Link>
+          </li>
+          <li>
+            <Link href="/lueftung">Lüftungsbereich</Link>
+          </li>
+          <li>
+            <Link href="/elektro">Elektrobereich</Link>
+          </li>
+        </StyledList>
+      )}
     </nav>
   );
 }
