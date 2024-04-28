5dc2440e602f1b73b3177a143edd1028a5494930
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2024-04-28 16:25:32 +0200

    change font size for large devices

--- components/Product/index.js
+++ components/Product/index.js
@@ -40,7 +40,6 @@ const StyledButton = styled.button`
   width: 100%;
   height: 100%;
   border: none;
-  gap: 2rem;
   cursor: pointer;
   &:hover,
   &:focus {
@@ -64,13 +63,16 @@ const TextWrapper = styled.div`
   flex-direction: column;
   margin-right: 1rem;
   h3 {
+    /* font-size: var(--large-product-headline); */
     font-size: var(--large-product-headline);
   }
   p {
+    /* font-size: var(--large-product-description); */
     font-size: var(--large-product-description);
     padding-right: 1rem;
   }
-  @media (max-width: 1000px) {
+
+  @media (max-width: 1400px) {
     h3 {
       font-size: var(--medium-product-headline);
     }
@@ -91,12 +93,17 @@ const TextWrapper = styled.div`
 const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
+  justify-self: center;
   align-items: center;
   background-color: white;
   border-radius: 50%;
   margin-left: 1rem;
-  width: 80px;
-  height: 80px;
+  width: 100px;
+  height: 100px;
+  @media (max-width: 1400px) {
+    max-width: 80px;
+    max-height: 80px;
+  }
   @media (max-width: 650px) {
     max-width: 60px;
     max-height: 60px;
@@ -106,6 +113,7 @@ const ImageWrapper = styled.div`
 const StyledImage = styled(Image)`
   width: 100%;
   height: 100%;
+  align-self: center;
   font-size: 0.5rem;
   border-radius: 50%;
   overflow: hidden;
--- components/ProductList/index.js
+++ components/ProductList/index.js
@@ -1,7 +1,4 @@
-import {
-  useEffect,
-  useState,
-} from 'react';
+import { useEffect, useState } from 'react';
 
 import styled from 'styled-components';
 
@@ -67,7 +64,7 @@ export default ProductList;
 
 const StyledListItem = styled.li`
   width: 100%;
-  height: 100px;
+  height: 125px;
   background-color: var(--background-category-color);
   border: none;
   border-radius: 5px;
@@ -79,5 +76,8 @@ const StyledListItem = styled.li`
   &:hover,
   &:focus {
     background-color: var(--background-category-hover-color);
+  }
+  @media (max-width: 1400px) {
+    height: 100px;
   }
 `;
