3b24a6ce83a2756c57d936c4d50b58428b567020
Eugen Moser <92641651+EugenMoser@users.noreply.github.com>
2025-05-01 14:02:33 +0200

    add categories keywords

--- helpers/constants.js
+++ helpers/constants.js
@@ -7,13 +7,39 @@ import {
 } from '@mdi/js';
 
 export const sections = [
-  { category: "moebel", name: "Möbelbereich", icon: mdiTableFurniture },
+  {
+    category: "moebel",
+    name: "Möbelbereich",
+    icon: mdiTableFurniture,
+    keywords:
+      "Baumann Tilo, Tilo, Wilfried Baumann, Wilfried, Spritzguss, Baumann Spritzgussteile, Kunststoffspritzgussteile, Kunststoffspritzguss, Kunststoffspritzerei, Spritzerei, Werkzeugbau, Formenbau, Entwicklung, Kunststoff, Gestratz, Möbelbereich, Möbelkunststoffteile, Möbelspritzgussteile, Möbelteile, Möbelfabrik, Möbelbau, Schreinereibedarf, Schreinerbedarf, Möbelbauer, Serienmöbel, Wohnwagenmöbel, Caravanmöbel, Wohnwagenspritzgussteile, Hersteller, Wohnwagenkunststoffteile, Wohnwagenteile, Wohnmobilspritzgussteile, Wohnmobilkunststoffteile, Wohnmobilteile, Caravankunststoffteile, Caravanausbauteile, Caravanspritzgussteile, Caravanteile, Motorcaravankunststoffteile, Motorcaravanspritzgussteile, Motorcaravanteile, Yachtkunststoffteile, Yachtspritzugussteile, Yachtteile, Caravanzubehör, Campingzubehör, Reisemobilzubehör, Wohnwagenzubehör, Wohnmobilzubehör, Tineyhouse, Mobile Home, Wohncontainer, Sanitärcontainer, Flüchtlingsunterkunft, Bootkunststoffteile, Bootspritzugussteile, Bootteile, Gartenhaus,",
+  },
   {
     category: "halterung",
     name: "Halterungsbereich",
     icon: mdiTournament,
+    keywords:
+      "Baumann Tilo, Tilo, Wilfried Baumann, Wilfried, Spritzguss, Baumann Spritzgussteile, Kunststoffspritzgussteile, Kunststoffspritzguss, Kunststoffspritzerei, Spritzerei, Werkzeugbau, Formenbau, Entwicklung, Hersteller, Kunststoff, Gestratz, Halterungsbereich, Kunststoffhalterung, Spritzgusshalter, Befestigungsteile, Sanitärteile, Befestigung, Schelle, Fixierung, Fixierhilfe, Hydraulik, Wasser, Gas, Druckluft, Wohnwagenspritzgussteile, Wohnwagenkunststoffteile, Wohnwagenteile, Wohnmobilspritzgussteile, Wohnmobilkunststoffteile, Wohnmobilteile, Caravankunststoffteile, Caravanspritzgussteile, Caravanausbauteile, Caravanteile, Motorcaravankunststoffteile, Motorcaravanspritzgussteile, Motorcaravanteile, Yachtkunststoffteile, Yachtspritzugussteile, Yachtteile, Caravanzubehör, Campingzubehör, Reisemobilzubehör, Wohnwagenzubehör, Wohnmobilzubehör, Tineyhouse, Mobile Home, Wohncontainer, Sanitärcontainer, Flüchtlingsunterkunft, Bootkunststoffteile, Bootspritzugussteile, Bootteile, Gartenhaus,",
   },
-  { category: "wasser", name: "Wasserbereich", icon: mdiWaterOutline },
-  { category: "lueftung", name: "Lüftungsbereich", icon: mdiAirFilter },
-  { category: "elektro", name: "Elektrobereich", icon: mdiFlashOutline },
+  {
+    category: "wasser",
+    name: "Wasserbereich",
+    icon: mdiWaterOutline,
+    keywords:
+      "Baumann Tilo, Tilo, Wilfried Baumann, Wilfried, Spritzguss, Baumann Spritzgussteile, Kunststoffspritzgussteile, Kunststoffspritzguss, Kunststoffspritzerei, Spritzerei, Werkzeugbau, Formenbau, Entwicklung, Hersteller, Kunststoff, Gestratz, Wasserbereich, Wassertank, Frischwassertank, Brauchwasser, Kaltwasser, Frischwasser, Trinkwasser,Dichtungen, Dichtungstechnik, Abwassertank, Wasserleitung, Siphon, Sifon, Ablauf, Ventil, Sanitärteile, KTW, KTW-BWGL, DVGW, W-270, DIN EN 16421, Wohnwagenspritzgussteile, Wohnwagenkunststoffteile, Wohnwagenteile, Wohnmobilspritzgussteile, Wohnmobilkunststoffteile, Wohnmobilteile, Caravankunststoffteile, Caravanausbauteile, Caravanspritzgussteile, Caravanteile, Motorcaravankunststoffteile, Motorcaravanspritzgussteile, Motorcaravanteile, Yachtkunststoffteile, Yachtspritzugussteile, Yachtteile, Caravanzubehör, Campingzubehör, Reisemobilzubehör, Wohnwagenzubehör, Wohnmobilzubehör, Tineyhouse, Mobile Home, Wohncontainer, Sanitärcontainer, Flüchtlingsunterkunft, Bootkunststoffteile, Bootspritzugussteile, Bootteile, Gartenhaus,",
+  },
+  {
+    category: "lueftung",
+    name: "Lüftungsbereich",
+    icon: mdiAirFilter,
+    keywords:
+      "Baumann Tilo, Tilo, Wilfried Baumann, Wilfried, Spritzguss, Baumann Spritzgussteile, Kunststoffspritzgussteile, Kunststoffspritzguss, Kunststoffspritzerei, Spritzerei, Werkzeugbau, Formenbau, Entwicklung, Hersteller, Kunststoff, Gestratz, Lüftungsbereich, Heizung, Belüftung, Klimaanlage, Standheizung, Wohnwagenspritzgussteile, Wohnwagenkunststoffteile, Wohnwagenteile, Wohnmobilspritzgussteile, Wohnmobilkunststoffteile, Wohnmobilteile, Caravankunststoffteile, Caravanspritzgussteile, Caravanteile, Motorcaravankunststoffteile, Motorcaravanspritzgussteile, Motorcaravanteile, Yachtkunststoffteile, Yachtspritzugussteile, Yachtteile, Caravanzubehör, Caravanausbauteile, Campingzubehör, Reisemobilzubehör, Wohnwagenzubehör, Wohnmobilzubehör, Tineyhouse, Mobile Home, Wohncontainer, Sanitärcontainer, Flüchtlingsunterkunft, Bootkunststoffteile, Bootspritzugussteile, Bootteile, Gartenhaus,",
+  },
+  {
+    category: "elektro",
+    name: "Elektrobereich",
+    icon: mdiFlashOutline,
+    keywords:
+      "Baumann Tilo, Tilo, Wilfried Baumann, Wilfried, Spritzguss, Baumann Spritzgussteile, Kunststoffspritzgussteile, Kunststoffspritzguss, Kunststoffspritzerei, Spritzerei, Werkzeugbau, Formenbau, Entwicklung, Hersteller, Kunststoff, Gestratz, Elektrobereich,  Gehäuse, Platinengehäuse, Dose, Abdeckung, Leuchte, Abdeckdose, Wohnwagenspritzgussteile, Wohnwagenkunststoffteile, Wohnwagenteile, Wohnmobilspritzgussteile, Wohnmobilkunststoffteile, Wohnmobilteile, Caravankunststoffteile, Caravanspritzgussteile, Caravanteile, Motorcaravankunststoffteile, Motorcaravanspritzgussteile, Motorcaravanteile, Yachtkunststoffteile, Yachtspritzugussteile, Yachtteile, Caravanzubehör, Caravanausbauteile, Campingzubehör, Reisemobilzubehör, Wohnwagenzubehör, Wohnmobilzubehör, Tineyhouse, Mobile Home, Wohncontainer, Sanitärcontainer, Flüchtlingsunterkunft, Bootkunststoffteile, Bootspritzugussteile, Bootteile, Gartenhaus,",
+  },
 ];
